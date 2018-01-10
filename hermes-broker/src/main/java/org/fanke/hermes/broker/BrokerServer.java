ec4ec37271622950637b65bc45501c96ddb3ff15
chao nie <chaonie@chaodeMacBook-Pro.local>
2018-01-10 14:03:50 +0800

    first commit

---
+++ hermes-broker/src/main/java/org/fanke/hermes/broker/BrokerServer.java
@@ -0,0 +1,15 @@
+package org.fanke.hermes.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chao.nie@weimob.com
+ */
+@SpringBootApplication
+public class BrokerServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokerServer.class,args);
+    }
+}
